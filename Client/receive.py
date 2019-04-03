021554b18897f5096c4d6b384a5a074a20b4360e
VMinhoto <vitor.v.minhoto@gmail.com>
2019-04-03 12:04:29 +0100

    save events csv, fixed 'none' problem on pulling markers

--- Client/receive.py
+++ Client/receive.py
@@ -2,13 +2,13 @@ from pylsl import StreamInlet, resolve_stream
 
 # first resolve an EEG stream on the lab network
 print("looking for an EEG stream...")
-streams = resolve_stream('type', 'EEG')
+streams = resolve_stream('type', 'Markers')
 
 # create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
 info = inlet.info()
-print("The stream's XML meta-data is: ")
-print(info.as_xml())
+#print("The stream's XML meta-data is: ")
+#print(info.as_xml())
 
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
--- Client/save.py
+++ Client/save.py
@@ -11,38 +11,44 @@ import pickle
 z=Experiment("Vitor",1)
 x=500
 stopvar=500
+mysampleEvent=None
+timestampEvent=None
 
 def experiment2csv(experiment):
     pickle.dump( experiment, open( "save.p", "wb" ) )
 
 def signal_handler(signal, frame):
-    a=z
-    a.saveCSV()
+    z.saveEEGCSV()
+    z.saveMarkerCSV()
     sys.exit(0)
 
 print("looking for an EEG stream...")
 streams = resolve_stream('type', 'EEG')
+print("Found EEG Stream")
+print("looking for an Markers stream...")
+stream2 = resolve_stream('type', 'Markers')
+print("Found Markers Stream")
 
 # create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
-
+inletevent = StreamInlet(stream2[0])
 signal.signal(signal.SIGINT, signal_handler)
-while inlet.info():
-    # make a new random 8-channel sample; this is converted into a
-    # pylsl.vectorf (the data type that is expected by push_sample)
-    mysample, timestamp = inlet.pull_sample()
-    #mysample = [rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()]
-    insertEEGSample=Sample(timestamp,mysample)
-    """ if x==stopvar:
-        insertEventSample=Sample(int(round(time.time() * 1000)),1)
-        z.addEventSample(insertEventSample)
-        stopvar-=50 """
+while 1:
 
-    # now send it and wait for a bit
+    # Pull EEG sample, convert to EEG object and append to EEG array
+    mysample, timestamp = inlet.pull_sample()
+    insertEEGSample=Sample(timestamp,mysample)
+    # print(mysample)
     z.addEEGSample(insertEEGSample)
-    time.sleep(0.01)
-    print(x)
-    
+
+    # Pull Marker sample, convert to Marker object and append to Events array
+    mysampleEvent, timestampEvent = inletevent.pull_sample(timeout=0.0)
+    if mysampleEvent is not None:
+        insertEventSample=Sample(timestampEvent,mysampleEvent)
+        print(mysampleEvent)
+        z.addEventSample(insertEventSample)
+
+     
 
 while 1:
     a=0
